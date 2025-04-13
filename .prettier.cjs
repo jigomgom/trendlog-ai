1e41d9ccad3aac9b585e361f88f483efec2283af
jigomgom <107230384+jigomgom@users.noreply.github.com>
2025-04-13 13:53:40 +0900

    setting: prettier 추가 (#1) (#6)

---
+++ .prettier.cjs
@@ -0,0 +1,12 @@
+/* eslint-disable */
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  endOfLine: "auto",
+  printWidth: 100,
+  arrowParens: "always",
+  trailingComma: "all",
+  useTabs: false,
+};
